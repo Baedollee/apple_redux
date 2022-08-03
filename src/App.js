6feef4cf18e21475da4d131e90a1e5a816e5106c
baedollee <baesee0401@gmail.com>
2022-08-04 01:46:23 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider,useSelector } from 'react-redux';
+
+
+const reducer(state, action){
+  return state
+}
+const initialState = {value:0}
+const store = createStore(reducer, initialState)
+
+const Counter = () => {
+  const count = useSelector(state=>state.value)
+  return (
+    <div>
+      <button>+</button>{count}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
   );
 }
 
